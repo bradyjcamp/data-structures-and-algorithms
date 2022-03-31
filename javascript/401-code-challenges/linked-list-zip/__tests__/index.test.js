523497564f966082aa5ec2f42260efacdf2f6164
Brady Camp <bradyjcamp@gmail.com>
2022-03-30 20:14:36 -0400

    completed code challenge 8

---
+++ javascript/401-code-challenges/linked-list-zip/__tests__/index.test.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const { LinkedList, Node } = require('../index');
+
+
+describe('LINKED-LIST-ZIP', () => {
+  let llA = new LinkedList();
+  let llB = new LinkedList();
+
+  it('should return one zipped Linked List if two are given', () => {
+    llA.append(1);
+    llA.append(2);
+    llA.append(3);
+    llB.append(4);
+    llB.append(5);
+    llB.append(6);
+    llA.zipList(llA, llB);
+    expect(llA.toString()).toBe('{ 1 } -> { 4 } -> { 2 } -> { 5 } -> { 3 } -> { 6 } -> NULL');
+
+  });
+
+  it('Should return zipped Linked List if one is larger than the other', () => {
+    llA.append(1);
+    llA.append(2);
+    llB.append(4);
+    llB.append(5);
+    llB.append(6);
+    llA.zipList(llA, llB);
+    expect(llA.toString()).toBe('{ 1 } -> { 4 } -> { 2 } -> { 5 } -> { 6 } -> NULL');
+  });
+});
